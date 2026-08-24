9a6398979a49fb1e803b533d1bf1713885f10698
Shristi Mohan Verma <shristimohanverma9182@gmail.com>
2026-08-24 11:37:15 +0530

    ✅ JASSIGNMENTS - Rating 513 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/easy/JASSIGNMENTS---Rating-513/solution.cpp
+++ codechef/easy/JASSIGNMENTS---Rating-513/solution.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main() {
 	// your code goes here
+	ios_base::sync_with_stdio(false);
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int t=3;
+	    int d=10-x;
+	    if(d<3)
+	    cout<<"No\n";
+	    else
+	    cout<<"Yes\n";
+	}
 
 }
