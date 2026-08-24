0e8868081d0721b333e555e8ad2564b33d362bcf
Shristi Mohan Verma <shristimohanverma9182@gmail.com>
2026-08-24 11:24:51 +0530

    ✅ SUBSCRIBE_ - Rating 504 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ codechef/easy/SUBSCRIBE_---Rating-504/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int T;
+	cin>>T;
+	while(T--){
+	    int N , X;
+	    cin>>N>>X;
+	    
+	}
+
+}
